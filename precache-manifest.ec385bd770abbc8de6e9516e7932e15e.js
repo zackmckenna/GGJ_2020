f477200015a242118c4152f5a83354fb1c283eda
Zack McKenna <zackmckenna@gmail.com>
2020-01-31 14:21:19 -0500

    Updates

---
+++ precache-manifest.ec385bd770abbc8de6e9516e7932e15e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402e058a23cb55203ac42ba948ab2340",
+    "url": "/GGJ_2020/index.html"
+  },
+  {
+    "revision": "f1fd0bb0b89b2bb6f98e",
+    "url": "/GGJ_2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a47dd75a6f63d1f0bb9",
+    "url": "/GGJ_2020/static/js/2.38428891.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/GGJ_2020/static/js/2.38428891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fd0bb0b89b2bb6f98e",
+    "url": "/GGJ_2020/static/js/main.4609d6ba.chunk.js"
+  },
+  {
+    "revision": "e2bf22652e072e7cfa76",
+    "url": "/GGJ_2020/static/js/runtime-main.d8aba86a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GGJ_2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
