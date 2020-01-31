98fcacced5d9d53e02fb12b38201b9242b6b885d
Zack McKenna <zackmckenna@gmail.com>
2020-01-31 15:07:32 -0500

    gg logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import ggjLogo from './ggjLogo.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ggjLogo} className="App-logo" alt="logo" />
         <p>
           <code>gearing up for GGJ 2020!!!!</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
