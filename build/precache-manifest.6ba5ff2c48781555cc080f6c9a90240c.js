5c91b803576025daf6d767afbc89100814962539
Zack McKenna <zackmckenna@gmail.com>
2020-02-01 15:16:15 -0500

    new build

---
+++ build/precache-manifest.6ba5ff2c48781555cc080f6c9a90240c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abeb1810819f7d5fc979bdab81c182d",
+    "url": "/GGJ_2020/index.html"
+  },
+  {
+    "revision": "1d5cde358da7e0bddb64",
+    "url": "/GGJ_2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c4dd24cb8176ec65e144",
+    "url": "/GGJ_2020/static/css/main.47c80d1f.chunk.css"
+  },
+  {
+    "revision": "1d5cde358da7e0bddb64",
+    "url": "/GGJ_2020/static/js/2.fe5091f8.chunk.js"
+  },
+  {
+    "revision": "524f0c03e82a7e15116f065039fb5ec1",
+    "url": "/GGJ_2020/static/js/2.fe5091f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4dd24cb8176ec65e144",
+    "url": "/GGJ_2020/static/js/main.31fdeac6.chunk.js"
+  },
+  {
+    "revision": "e2bf22652e072e7cfa76",
+    "url": "/GGJ_2020/static/js/runtime-main.d8aba86a.js"
+  }
+]);
