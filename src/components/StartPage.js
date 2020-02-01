e6c87920bd8caf98e600a0048435cfc6e839fe49
Bryan Barnes <bbarnes@lutron.com>
2020-02-01 12:58:42 -0500

    added premise/story to the start page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -29,6 +29,21 @@ const StartPage = ({
       <img src={getRandomImage(introPix, '/images/intros/')} className="App-logo" alt="logo" />
       <p></p>
         <code>GGJ 2020</code>
+      
+<p>Welcome to Space Station Gornath! What brings you this deep into the Omega Quadrant?
+Anyways, the rough conditions and remoteness of this region make this output an oasis for explorers. Most visitors have a need for repairs and upgrades. 
+Due to this need multiple repair shops have emerged in the station, of varying quality…</p>
+
+<p>In this game you will take turns being the <u>repair shop owners</u> and <u>customers</u> with particular needs. </p>
+
+<p>As the <u>repair shop owner</u>, the sale is all that matters - the almighty space buck is king. You will need to work with what you have to engineer a solution and out-sell the rival shops. </p>
+
+<p>As the <u>customer</u>, you will decide which solution to purchase based on its ingenuity and how well it solves your problem. In these times, it doesn’t take much to get a ship, and most pilots aren’t experts in engineering.</p>
+
+<p>Get ready to craft some space stuff!</p>
+<p></p>
+<p></p>
+
       <p>How many players?</p>
     <Form onChange={event => handlePlayerChange(event)}>
     <Form.Group controlId="exampleForm.ControlSelect2">
