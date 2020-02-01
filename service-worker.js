d9c1609c2f44e0148fe14bf8af511d8727560492
Zack McKenna <zackmckenna@gmail.com>
2020-02-01 15:23:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GGJ_2020/precache-manifest.2e2a97edc4d1bb27c1ff614fa86addf4.js"
+  "/GGJ_2020/precache-manifest.6ba5ff2c48781555cc080f6c9a90240c.js"
 );
 
 self.addEventListener('message', (event) => {
