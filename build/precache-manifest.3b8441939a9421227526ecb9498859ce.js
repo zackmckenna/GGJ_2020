cec5b5d5cb270e779e7deef0209f9fbe48345393
Zack McKenna <zackmckenna@gmail.com>
2020-02-01 15:04:10 -0500

    feb1 deploy

---
+++ build/precache-manifest.3b8441939a9421227526ecb9498859ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52b7a99d95615a748b84c031ffcc488",
+    "url": "/GGJ_2020/index.html"
+  },
+  {
+    "revision": "4694c2fda2720c612424",
+    "url": "/GGJ_2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "30da0854f1ea70373acf",
+    "url": "/GGJ_2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4694c2fda2720c612424",
+    "url": "/GGJ_2020/static/js/2.487f0658.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/GGJ_2020/static/js/2.487f0658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30da0854f1ea70373acf",
+    "url": "/GGJ_2020/static/js/main.7a8c1267.chunk.js"
+  },
+  {
+    "revision": "e2bf22652e072e7cfa76",
+    "url": "/GGJ_2020/static/js/runtime-main.d8aba86a.js"
+  },
+  {
+    "revision": "46710f3bf34c2a4b25fd4b571a6842ae",
+    "url": "/GGJ_2020/static/media/ggjLogo.46710f3b.svg"
+  }
+]);
