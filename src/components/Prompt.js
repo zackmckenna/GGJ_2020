3516681cf9b387394d2034603a13d8aeb40b842d
Bryan Barnes <bbarnes@lutron.com>
2020-02-01 12:48:13 -0500

    added intro image to start screen

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'react-bootstrap'
 
+//todo:
+//-generate prompt from airtable randomw rows; "I need a __(part)__ that __(criteria)__"
+//-display a random prompt image
+//-indicate which player is the judge
+
 const Prompt = ({ players }) => {
   return (
     <>
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
-import ggjLogo from '../ggjLogo.svg'
+
 import { Button, Form } from 'react-bootstrap'
 import { withRouter, Link } from 'react-router-dom'
+
+
+var introPix = new Array("STATION.BMP","TEMPLE.BMP","ASTEROID.BMP");
+function getRandomImage(imgAr, path) {
+    path = path || '/images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = process.env.PUBLIC_URL + path + img;
+    return imgStr;
+}
 
 const StartPage = ({
   handlePlayerChange,
@@ -15,11 +25,11 @@ const StartPage = ({
 
   return (
     <>
-    Start Page
-      <img src={ggjLogo} className="App-logo" alt="logo" />
+    Space Station Gizmo Guy!
+      <img src={getRandomImage(introPix, '/images/intros/')} className="App-logo" alt="logo" />
       <p></p>
         <code>GGJ 2020</code>
-      <p>How many players ?</p>
+      <p>How many players?</p>
     <Form onChange={event => handlePlayerChange(event)}>
     <Form.Group controlId="exampleForm.ControlSelect2">
     <Form.Label>Select Number of Players</Form.Label>
