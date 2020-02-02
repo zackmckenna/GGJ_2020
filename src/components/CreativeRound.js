975a4fcafffb830d16687dae4e1211064004acbc
Bryan Barnes <bbarnes@lutron.com>
2020-02-01 23:23:27 -0500

    fixed randomization

--- src/components/CreativeRound.js
+++ src/components/CreativeRound.js
@@ -23,8 +23,15 @@ const CreativeRound = ({ timerCounter, setTimerCounter, players, playerNames, pl
   console.log(timerCounter)
 
   const roundPlayers = playerObjects.map(player => {
+    var compVals = new Set()
     for (let i = 0; i < 6; i++) {
-      player.components[i] = components[Math.floor(Math.random() * components.length)]
+      var rand
+      do{
+        rand = Math.floor(Math.random() * components.length);
+        compVals.add(rand)
+      }while(!compVals.has(rand))
+      
+      player.components[i] = components[rand]
     }
   })
 
