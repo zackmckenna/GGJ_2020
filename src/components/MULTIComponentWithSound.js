ffa4d8b13bee1571f88f373b641d838be88241b6
Bryan Barnes <bbarnes@lutron.com>
2020-02-03 00:18:25 -0500

    fixed new variation of sound reloading bug

--- src/components/MULTIComponentWithSound.js
+++ src/components/MULTIComponentWithSound.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import Sound from 'react-sound';
 
 
@@ -6,41 +6,46 @@ const Chords = new Array("CHORDS1.mp3", "CHORDS2.mp3", "CHORDS3.mp3");
 const Drums = new Array("DRUMS1.mp3","DRUMS2.mp3","Drums3.mp3");
 const FX = new Array("FX1.mp3","FX2.mp3","FX3.mp3")
 
+
+
 function getSound(name, path) {
     path = path || '/music/stems/'; // default path here
     var soundStr = process.env.PUBLIC_URL + path + name;
     return soundStr;
 }
 
-class MULTIComponentWithSound extends React.Component {
 
-  render() {
+
+function MULTIComponentWithSound(){
+
+  const[chordsTrack, setChordsTrack] = useState(getSound(Chords[Math.floor( Math.random() * Chords.length )]))
+  const[drumsTrack, setDrumsTrack] = useState(getSound(Drums[Math.floor( Math.random() * Drums.length )]))
+  const[fxTrack, setFxTrack] = useState(getSound(FX[Math.floor( Math.random() * FX.length )]))
+
+  useEffect(() => {
+      setChordsTrack(getSound(Chords[Math.floor( Math.random() * Chords.length )]))
+      setDrumsTrack(getSound(Drums[Math.floor( Math.random() * Drums.length )]))
+      setFxTrack(getSound(FX[Math.floor( Math.random() * FX.length )]))
+    }, [])
+
+
     return (<>
     <Sound
-      url={getSound(Chords[Math.floor( Math.random() * Chords.length )])}
+      url={chordsTrack}
       playStatus={Sound.status.PLAYING}
-      onLoading={this.handleSongLoading}
-      onPlaying={this.handleSongPlaying}
-      onFinishedPlaying={this.handleSongFinishedPlaying}
       loop={true}
     />
     <Sound
-      url={getSound(Drums[Math.floor( Math.random() * Drums.length )])}
+      url={drumsTrack}
       playStatus={Sound.status.PLAYING}
-      onLoading={this.handleSongLoading}
-      onPlaying={this.handleSongPlaying}
-      onFinishedPlaying={this.handleSongFinishedPlaying}
       loop={true}
     />
     <Sound
-      url={getSound(FX[Math.floor( Math.random() * FX.length )])}
+      url={fxTrack}
       playStatus={Sound.status.PLAYING}
-      onLoading={this.handleSongLoading}
-      onPlaying={this.handleSongPlaying}
-      onFinishedPlaying={this.handleSongFinishedPlaying}
       loop={true}
     /></>
   );
-  }
+
 }
 export default MULTIComponentWithSound
