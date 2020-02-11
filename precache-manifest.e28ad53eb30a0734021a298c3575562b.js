e3d6de55439384f583021de40acdb27b9679ac43
Zack McKenna <zackmckenna@gmail.com>
2020-02-11 13:32:25 -0500

    Updates

--- precache-manifest.e28ad53eb30a0734021a298c3575562b.js
+++ precache-manifest.e28ad53eb30a0734021a298c3575562b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed60dce8d2526c909ff55288c1f1a5be",
+    "revision": "2fd9f18ff060be4898d947efe19042f7",
     "url": "/GGJ_2020/index.html"
   },
   {
-    "revision": "e750c010f06251a8fa41",
+    "revision": "5d7313ec7f81436bb3a0",
     "url": "/GGJ_2020/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "530dc69b310b7eafe982",
+    "revision": "768da067ab07685340e1",
     "url": "/GGJ_2020/static/css/main.47c80d1f.chunk.css"
   },
   {
-    "revision": "e750c010f06251a8fa41",
-    "url": "/GGJ_2020/static/js/2.22061a6b.chunk.js"
+    "revision": "5d7313ec7f81436bb3a0",
+    "url": "/GGJ_2020/static/js/2.4ecb3cd8.chunk.js"
   },
   {
     "revision": "524f0c03e82a7e15116f065039fb5ec1",
-    "url": "/GGJ_2020/static/js/2.22061a6b.chunk.js.LICENSE.txt"
+    "url": "/GGJ_2020/static/js/2.4ecb3cd8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "530dc69b310b7eafe982",
-    "url": "/GGJ_2020/static/js/main.b5a4d5a0.chunk.js"
+    "revision": "768da067ab07685340e1",
+    "url": "/GGJ_2020/static/js/main.597c1e5f.chunk.js"
   },
   {
     "revision": "e2bf22652e072e7cfa76",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GGJ_2020/precache-manifest.140e19b1f1f212c69ea88b9840fbaa79.js"
+  "/GGJ_2020/precache-manifest.e28ad53eb30a0734021a298c3575562b.js"
 );
 
 self.addEventListener('message', (event) => {
